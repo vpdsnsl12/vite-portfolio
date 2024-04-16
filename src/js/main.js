a7334d7c51c6f20c2bc7b39099ca930ae40c8a49
vpdsnsl12 <vpdsnsl123@naver.com>
2024-04-16 11:40:51 +0900

    기초 환경설정

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+import { graphics } from "./graphics.js";
+import { intro } from "./intro.js";
+import { link } from "./link.js";
+import { menu } from "./menu.js";
+import { smooth } from "./smooth.js";
+import { work } from "./work.js";
+
+window.addEventListener('load',function(){
+  smooth()
+  graphics()
+  link()
+  menu()
+  intro()
+  work()
+})
