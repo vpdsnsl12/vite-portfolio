00db229a9353f743dc7a3b256ad5094e81779765
vpdsnsl12 <vpdsnsl123@naver.com>
2024-04-17 14:16:35 +0900

    header + intro작업물추가

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,21 @@
-export function intro(){
-    
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+export function intro() {
+    gsap.registerPlugin(ScrollTrigger)
+    const frameConunt = 7
+    let offsetValue = 358
+
+
+    gsap.to('.img', {
+        backgroundPosition: (-offsetValue* frameConunt) + 'px',
+        ease: 'steps(' + frameConunt + ')',
+        scrollTrigger: {
+            trigger: '#intro',
+            start: 'top top',
+            end: '+=' + (frameConunt * offsetValue),
+            pin: true,
+            scrub: true
+        }
+    })
 }
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,16 @@
 export function menu(){
-    
+    const headerToggle=document.getElementById('headerToggle')
+    const headerNav=document.querySelector('.header_nav')
+
+    if(headerToggle){
+        headerToggle.addEventListener('click',function(){
+            headerNav.classList.toggle('show')
+            // 조건식
+            headerToggle.getAttribute("aria-expanded")=='true'
+            // true이면
+            ?headerToggle.setAttribute('aria-expanded','false')
+            // false이면
+            : headerToggle.setAttribute('aria-expanded','true')
+        })
+    }
 }
