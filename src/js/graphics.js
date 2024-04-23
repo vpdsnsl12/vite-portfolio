4d66b054646db8d3828574b0543367f68097994e
vpdsnsl12 <vpdsnsl123@naver.com>
2024-04-23 10:34:08 +0900

    그래픽 페이지 완성

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -1,3 +1,24 @@
-export function graphics(){
-    
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function graphics() {
+    gsap.utils.toArray('.img_box').forEach(function (imgBox) {
+        gsap.timeline({ scrollTrigger:{
+                trigger: imgBox,
+                start: '50% 100%',
+                toggleClass: { 'targets': imgBox, className: 'active' },
+                scrub: 1
+            }})
+    })
+    gsap.utils.toArray('.img_text').forEach(function (imgText) {
+        gsap.timeline({scrollTrigger: {
+                trigger: imgText,
+                start: '50% 90%',
+                end: '100% 0%',
+                toggleClass: { 'targets': imgText, className: 'active' },
+                scrub: 1
+            }})
+    })
 }
